b5f32b67bec9245521b089b139e26d9a1bd5afff
LucaMonetti <63344053+LucaMonetti@users.noreply.github.com>
2021-04-25 20:42:01 +0200

    Create Level-8.py

---
+++ Level-8.py
@@ -0,0 +1,14 @@
+// Import MD5 
+
+// Funzione di controllo
+
+def Verify():
+	return
+
+key = 0
+
+while key !== 10**5:
+	if (verify(key * 'resto Chiave')):
+		print('La chive è: ', key)
+		return
+	key += 1
